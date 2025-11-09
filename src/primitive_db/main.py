f1323a20697dd80ba9f4a71b8f613cfe15f8bf1a
Denis39rus <carbovskydenis@yandex.ru>
2025-11-09 15:49:08 +0300

    Final check

--- src/primitive_db/main.py
+++ src/primitive_db/main.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
+from .engine import run
+
 
 def main():
-   from src.primitive_db.engine import welcome
-   welcome()
+    print('\nДоброе пожаловать в примитивную базу данных!\n')
+    run()
 
 if __name__ == "__main__":
     main()
+    
+    
